128e0d07c7759a591ea42aedd9bc8ca25823f040
Masood <masood@Masoods-MacBook-Pro.local>
2020-09-16 00:44:16 +0430

    http-ajax-00

---
+++ src/components/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Post.css'
+
+const post = (props) => (
+  <article className="post">
+    <h1>Title</h1>
+    <div>
+      <div className="author">Author</div>
+    </div>
+  </article>
+)
+
+export default post
